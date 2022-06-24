a99738e418cf454a8e49d13c0c156a23024d63e3
Neo <neo@neo.neo>
2022-06-24 14:40:38 +0800

    chore

--- src/dict.ts
+++ src/dict.ts
@@ -1,6 +1,6 @@
 import { workspace } from 'vscode'
 
-const userConfig = workspace.getConfiguration('html-entities-converter').get('overrides') as Record<string, string>
+const userConfig = workspace.getConfiguration('html-entities-converter').get<Record<string, string>>('overrides')
 export const dict: Record<string, string> = Object.assign({
   '<': '&lt;',
   '>': '&gt;',
